749dab6720a0dc03ab6b7d645399a70d481aa22a
Hello World !!! <anisharawat324@gmail.com>
2026-08-26 20:54:20 +0530

    Create (602)-friend-requests-ii-who-has-the-most-friends.sql

---
+++ (602)-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT id,count(*) as num
+FROM(
+    SELECT requester_id as id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id as id
+    FROM RequestAccepted
+) AS friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
